5dec0cf146316f1c3310892b600146d647208e26
mykhailo-metel <ukraine.blizzard@gmail.com>
2019-08-22 14:02:57 +0300

    003 Also class Test2

---
+++ homeWork/src/main/java/com/homework/Test2.java
@@ -0,0 +1,13 @@
+package com.homework;
+
+public class Test2 {
+    private boolean bool = false;
+
+    public boolean isBool() {
+        return bool;
+    }
+
+    public void setBool(boolean bool) {
+        this.bool = bool;
+    }
+}
